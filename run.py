255d5c41e1b3afeb6725e0a421d87e4c6c74580d
lestece <loiceleste94@gmail.com>
2022-12-10 17:55:16 +0000

    Build the tdee calculator function and import the whole module into the main run.py

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+import tdee_formulas
+
 # APIs to access in order for the program to run
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
--- tdee_formulas.py
+++ tdee_formulas.py
@@ -17,3 +17,55 @@ def bmr(lbm):
     and then adds 370 to it.
     """
     bmr = lbm * 21.6 + 370
+
+    return bmr
+
+
+def tef(bmr):
+    """
+    Calculates the Thermic Effect of Food.
+    It multiplies the BMR by 0.1
+    """
+    tef = bmr * 0.1
+    
+    return tef
+
+
+def tea(bmr, activity_level):
+    """
+    Calculates the Thermic Effect of Activity.
+    It first translates the activity level 
+    to a specific activity factor.
+    Then it multiplies this last one for the BMR
+    """
+    activity_factor = ""
+
+    if activity_level == "SED":
+        activity_factor = 1.2
+    elif activity_level == "LA":
+        activity_factor = 1.375
+    elif activity_level == "MA":
+        activity_factor = 1.55
+    elif activity_level == "VA":
+        activity_factor = 1.725
+    else:
+        activity_factor = 1.9
+    
+    tea = bmr * activity_factor
+
+    return tea
+
+
+def calculate_tdee(gender, weight, height, activity_level):
+    """
+    Calculates the Total Daily Energy Expenditure:
+    it sums up the bmr, tea and tef.
+    """
+    lbm = lbm(gender, int(weight), int(height))
+    bmr = bmr(lbm)
+    tef = tef(bmr)
+    tea(bmr, activity_level)
+
+    tdee = bmr + tea + tef
+
+    return tdee
